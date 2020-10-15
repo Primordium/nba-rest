12f17cd15fe8c9ee93b54dbfb649849776d4a427
primordium1 <primordium@gmail.com>
2020-10-15 10:41:23 +0100

    Spring boot base with H2 and MySQLconnector

---
+++ src/main/java/com/hro/exercise/nbachallenge/NbaChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.hro.exercise.nbachallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaChallengeApplication.class, args);
+	}
+
+}
